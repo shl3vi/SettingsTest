d2369c64d4eba537b688186ccaadaf24b18b63ac
shl3vi <shahar@sealights.io>
2016-08-23 16:44:44 +0300

    Add files via upload

---
+++ src/main/java/edu/lmu/cs/mike/impl/BitShiftDoubler.java
@@ -0,0 +1,16 @@
+package edu.lmu.cs.mike.impl;
+
+import edu.lmu.cs.mike.iface.IntegerDoubler;
+
+public class BitShiftDoubler implements IntegerDoubler {
+
+  @Override
+  public long doDouble(int i) {
+    return 0;
+  }
+
+  public long doDouble1(int i) {
+  	long a = 1+1;
+    return a;
+  }
+}
